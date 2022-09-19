91e0a575b5725fdc5237ecc4ebed8823871c2074
pustota-artem <pustota.artem@gmail.com>
2022-09-19 12:15:46 +0300

    message

--- src/script/info.js
+++ src/script/info.js
@@ -2,18 +2,15 @@
 
 const infoBlock = document.getElementById('info-block');
 const openBtn = document.getElementById('open-info-btn');
-const closeBtn = document.getElementById('close-info-btn')
 
 openBtn.addEventListener('click', function () {
-    infoBlock.classList.remove('max-h-0');
-    infoBlock.classList.add('max-h-60');
-    closeBtn.classList.remove('hidden');
-    openBtn.classList.add('hidden');
-})
-
-closeBtn.addEventListener('click', function () {
-    infoBlock.classList.remove('max-h-60'); 
-    infoBlock.classList.add('max-h-0');
-    closeBtn.classList.add('hidden');
-    openBtn.classList.remove('hidden');
-})
+    if (infoBlock.classList.contains('max-h-0')) {
+        infoBlock.classList.remove('max-h-0');
+        infoBlock.classList.add('max-h-[120rem]', 'duration-700');
+        openBtn.classList.add('rotate-180', 'duration-500');
+    } else {
+        infoBlock.classList.add('max-h-0');
+        infoBlock.classList.remove('max-h-[120rem]', 'duration-700');
+        openBtn.classList.remove('rotate-180');
+    }
+});
